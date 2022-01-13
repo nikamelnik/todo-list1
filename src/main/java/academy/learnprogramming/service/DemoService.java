dc5b18b6a607587dc3a2baf42199d5fc6e324e17
Veranika Melnik <zuakra@gmail.com>
2022-01-13 18:25:05 +0300

    created model package with 2 classes: TodoItem and TodoData

--- src/main/java/academy/learnprogramming/service/DemoService.java
+++ src/main/java/academy/learnprogramming/service/DemoService.java
@@ -3,5 +3,5 @@ package academy.learnprogramming.service;
 public interface DemoService {
 
     String getHelloMessage(String user);
-    //String getWelcomeMessage();
+    String getWelcomeMessage();
 }
