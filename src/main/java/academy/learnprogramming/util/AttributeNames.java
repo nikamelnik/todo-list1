3b46affc4b9987a6c66859a493a211956e430a47
Veranika Melnik <zuakra@gmail.com>
2022-01-17 17:10:27 +0300

    new: -home jsp -delete and editing items

---
+++ src/main/java/academy/learnprogramming/util/AttributeNames.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.util;
+
+public final class AttributeNames {
+    // == constants ==
+    public static final String TODO_ITEM = "todoItem";
+    // == constructors ==
+    private AttributeNames(){}
+}
